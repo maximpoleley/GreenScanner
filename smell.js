6f691d9f3a01fa26a5a755589ea7df6eca4be237
Maxim Poleley <maxim.po@moonactive.com>
2024-11-20 11:32:21 +0100

    Update smell.js

--- smell.js
+++ smell.js
@@ -1,27 +1,27 @@
 function name() {
   // ok: missing-template-string-indicator
-  return `this is ${start.line}`;
+  return this is ${start.line}
 }
 
 function ok() {
   // ok: missing-template-string-indicator
-  `test`;
+  test;
   if (true) { a = 3; }
-  `test`;
+  test;
 }
 
 function name2() {
   // ruleid: missing-template-string-indicator
-  return `this is ${start.line}`; // Fixed to use `${}` for interpolation
+  return this is {start.line}
 }
 
 function name3() {
   // ok: missing-template-string-indicator
-  return `this is ${start.line}`; // Fixed to use `${}` for interpolation
+  return "this is ${start.line}"
 }
 
-// If you meant to use a plain string instead:
+
 function name3() {
   // ok: missing-template-string-indicator
-  return "this is {start.line}"; // Keeps as a plain string
+  return "this is {start.line}"
 }
